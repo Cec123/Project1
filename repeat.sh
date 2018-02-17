2c733ce57009870e15301cf2f66e91d5454b87ca
Cecilia Furugård <cecilia.furugard@gmail.com>
2018-02-17 17:54:00 +0100

    uppdaterat organisering med att skapa mappar i git

--- repeat.sh
+++ repeat.sh
@@ -1,23 +1,30 @@
 #mkdir Project1
 #cd Project1
-touch readme.txt
+#touch readme.txt
  
 #A description of my project and what my predictor do. Maybe a short description of main steps on how the predictor was created.
 
 mkdir datasets
 #My sequences and other data that I will handle for my predictor.
+git add datasets
 
 mkdir scripts
 #all of my scripts, new script saved for every day having the date in the name of the scripts.
 
+git add scripts
+
 mkdir results
 #saving my results during the process of building of the code of my predictor.
+
+git add results
 
 mkdir doc
 #save documents like papers
 
+git add doc
 
 mkdir Notebook
 # date: what I did that day
 
+git add Notebook
 
