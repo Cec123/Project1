a75ed3c028fb87351282060e362855c4a359235a
Cecilia Furugård <cecilia.furugard@gmail.com>
2018-02-17 17:46:16 +0100

    organisera utkast1

---
+++ repeat.sh
@@ -0,0 +1,23 @@
+#mkdir Project1
+#cd Project1
+touch readme.txt
+ 
+#A description of my project and what my predictor do. Maybe a short description of main steps on how the predictor was created.
+
+mkdir datasets
+#My sequences and other data that I will handle for my predictor.
+
+mkdir scripts
+#all of my scripts, new script saved for every day having the date in the name of the scripts.
+
+mkdir results
+#saving my results during the process of building of the code of my predictor.
+
+mkdir doc
+#save documents like papers
+
+
+mkdir Notebook
+# date: what I did that day
+
+
