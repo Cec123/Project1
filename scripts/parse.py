daa87c3fd2a268e832ca8bed2f1114c7c1ae0b38
Cecilia Furugård <cecilia.furugard@gmail.com>
2018-02-27 15:51:47 +0100

    finished my X vector, Y-vector and trained with svm a model

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,6 +1,5 @@
 
 def parse_data(data):
-    
     filen = open(data, "r")
     data_topology = list()
     data_pepseq = list()
@@ -10,31 +9,18 @@ def parse_data(data):
     for nr, line in enumerate(filen):
         if line.startswith(">") == True:
             key = line[1:-1]
-            #keys_lista.append(line[1:-1])
+            keys_lista.append(line[1:-1])
             #print(keys_lista)
         elif nr %3 == 1:
             data_pepseq.append(line[:-1])
-    #print(data_pepseq)
+            #print(data_pepseq)
         elif nr%3 ==2:
             data_topology.append(line[:-1])
-
-            for element in data_topology:
-                bib_sec[key] = element
-            for element in data_pepseq:
-                bib_aa[key] = element
-                    
-                seq = bib_aa.values()
+            #print(data_topology)
     
-    #print(sequences)
-                    
-
-    filen.close()
-    return seq
 #-------------------------------------------------------------------------------
-def sliding_window(data):
+#Encode aminoacids to numbers:
     import numpy as np
-    filen = open(data, "r")
-    #print(filen)
 
     aminoacids = "ARNDCQEGHILKMFPSTWYV"
     aminoacids1 = "ARNDCQEGHILKMFPSTWYV"
@@ -42,14 +28,14 @@ def sliding_window(data):
     #print(key)
     
 #encoded aminoacids in matrix
-    matris = np.zeros((len(aminoacids), len(aminoacids1)))
+    matris = np.zeros((len(aminoacids), len(aminoacids1)), dtype=int)
     for i in range(len(aminoacids)):
         for j in range(len(aminoacids1)):
             if aminoacids[i] == aminoacids1[j]:
                 matris[i,j]=1        
-
     
     listakod =list()
+    
 #take out each row, put each row in a dictionary later with its aa as key.
     for arrays in matris:       
         listakod.append(arrays)
@@ -57,69 +43,98 @@ def sliding_window(data):
     bib1= dict(zip(key, listakod))
     bib1["0"] = np.zeros(20, dtype=int)
     #print(bib1)
-#---------------------------------
-
+#---------------------------------------------------------------------------------------
 #create sliding window
-
     win_size = 3
     pad = win_size//2
-    seq = "ASQRHIL"
-    windowlist= list()
 
-    for i in range(len(seq)):
-        #define interval in sequence were the first ans last letter in seq not included(pad=1):
-        if i>pad and i< len(seq)-pad:
-            #print(i)
-            #append each window to the list called window:
-            windowlist.append(seq[i-pad:i+pad+1])
-    #print(window)
-            
-    #handle the start:
-        elif i<= pad:
-            the_window = seq[:i + pad +1]
-            #print(the_window)
-            needzeros = win_size - len(the_window)
-            #print(needzeros)
-            windowlist.append("0"*needzeros + the_window)
-            #print(window)
-            
-    #handle the end:
-        else:
-            #print(i)
-            the_window = seq[i-1:]
-            #print(the_window)
-            needzeros = win_size - len(the_window)
-            #print(needzeros)
-            windowlist.append(the_window + "0"*needzeros)
-            #print(windowlist)
-#---------------------------------------------------------------
-#connvert my windowns to the codes of 1s and 0s.
-        #print(bib1)
-        map_windows = list()
+  #  seq = data_pepseq[0]
+    training_list = []
+    #print(data_pepseq)
+    for seq in data_pepseq:
+
+        # print(seq)
+        windowlist= list()
+
+        for i in range(len(seq)):
+    #define interval in sequence were the first ans last letter in seq not included(pad=1):
+            if i>pad and i< len(seq)-pad:
+                #print(i)
+                #append each window to the list called window:
+                windowlist.append(seq[i-pad:i+pad+1])
+        #print(window)
+                
+        #handle the start:
+            elif i<= pad:
+                the_window = seq[:i + pad +1]
+                #print(the_window)
+                needzeros = win_size - len(the_window)
+                #print(needzeros)
+                windowlist.append("0"*needzeros + the_window)
+                #print(window)
+                
+        #handle the end:
+            else:
+                #print(i)
+                the_window = seq[i-1:]
+                #print(the_window)
+                needzeros = win_size - len(the_window)
+                #print(needzeros)
+                windowlist.append(the_window + "0"*needzeros)
+                #print(windowlist)
+#--------------------------------------------------------------------------------
+    #connvert my windows to the codes of 1s and 0s.
 
         for elements in windowlist:
+            #print(elements)
             a = list()
             for letters in elements:
                 b = bib1[letters]
-                a.append(b)
-            a = [letters for elements in a for letters in elements]
-            map_windows.append(a)
-            print(map_windows)
-        #print(len(map_windows))
-        
-        
+                a.extend(b)
+            #print(a) 
+            training_list.append(a)
 
-        
-        
- 
-            
-            
+    #print(training_list)
+#------------------------------------------------------------------------------------
+# Create the y vector:
+    
+    topology_to_numbers = {".":1,"t":1,"S":2}
+    topology_prediction_nr = list()
+    #print(data_topology)
+
+    for elements in data_topology:
+        for letters in elements:
+            #print(letters)
+            y = topology_to_numbers[letters]
+            topology_prediction_nr.append(y)
+    print(topology_prediction_nr)
+#-----------------------------------------------------------------------
+#training
+    from sklearn import svm
+    X= np.array(training_list)
+    #print(X)
+    Y= topology_prediction_nr
+    #print(Y)       
+    clf = svm.SVC()
+    
+    clf.fit(X, Y)
+    result= clf.predict(X)
+    numbers_to_topology = {1:"M", 2:"S"}
+    topology_prediction_letter = list()
+
+    for numbers in result:
+        c = numbers_to_topology[numbers]
+        topology_prediction_letter.append(c)
+
+    print(topology_prediction_letter)
+
+#---------------------------------------------------------------------
             
 
 
 if __name__ == '__main__':
-    #parse_data("testa.txt")
-    sliding_window("kort.txt")
+    parse_data("short.txt")
+    
     
     
     
